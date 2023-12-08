dae20fc2b2d8b3e08957697c07720eea6d9325d4
ch3eSy <maxcoutanche@gmail.com>
2023-12-08 17:35:10 +0000

    Portal Animation + Level Building

--- src/entities/Portals.java
+++ src/entities/Portals.java
@@ -1,46 +1,57 @@
 package entities;
+import static utils.Constants.PlayerConstants.GetSpriteAmount;
+
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
 
 import javax.imageio.ImageIO;
 
 public class Portals extends entity{
-	private Image aniport;
+	private BufferedImage[][] aniport;
+    private int aniTick, aniIndex, aniSpd = 25;
 	public Portals(int x,int y,int w,int h) {
 		super(x,y,w,h);
 		portalsloadAnimations();
 	}
 	public void render(Graphics g) {
-		g.drawImage(aniport,(int)posx,(int)posy,(int)width,(int)height,null);
+		g.drawImage(aniport[0][aniIndex],(int)posx,(int)posy,(int)width,(int)height,null);
 	}
 	
-	private void portalsloadAnimations() {
-		InputStream is = getClass().getResourceAsStream("/portal.png");
-		try {
-			Image img = ImageIO.read(is);
-			aniport = img;
-		} catch (IOException e) {
-			e.printStackTrace();
-		}finally {
-			try {
-				is.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-	}
-	public void update() {
-		posx=0;
-		posy=0;
+    private void portalsloadAnimations() {
+        InputStream is = getClass().getResourceAsStream("/portal-sprites.png");
 
+        try {
+            BufferedImage img = ImageIO.read(is);
+            aniport = new BufferedImage[5][10];
+
+            for (int j = 0; j < aniport.length; j++)
+                for (int i = 0; i < aniport[j].length; i++)
+                    aniport[j][i] = img.getSubimage(i *128, j * 128, 128, 128);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+	public void update() {
+        updateAnimationTick();
 	}
 	public Rectangle getHitbox() {
 		return new Rectangle((int)posx-5,(int)posy-5,96,128);
 	}
-
+	
+    private void updateAnimationTick() {
+        aniTick++;
+        if (aniTick >= aniSpd) {
+            aniTick = 0;
+            aniIndex++;
+            if (aniIndex >= 10) {
+                aniIndex = 0;
+            }
+        }
+    }
     public double getX() {
         return posx;
     }
--- src/main/Game.java
+++ src/main/Game.java
@@ -23,7 +23,7 @@ public class Game implements Runnable{
 	private Thread gameThread;
 	private final int fps_max = 140;
 	private final int ups_max = 200;
-	private int level = 1;
+	public int level = 1;
     private List<floorTiles> floorTilesList;
 	private player Player;
 	private backdrop Backdrop;
@@ -61,15 +61,18 @@ public class Game implements Runnable{
         portalList = new ArrayList<>();
         if(level==1) {
         	for(i = 0; i<1920;i+=32) {
-                floorTilesList.add(new floorTiles(i, 1048, 32, 32));
+                spikeList.add(new Spikes(i, 1048, 32, 32));
         	}
-        	floorTilesList.add(new floorTiles(850, 920, 32, 32));
-        	spikeList.add(new Spikes(928,1016,32,32));
-        	spikeList.add(new Spikes(896,1016,32,32));
-        	portalList.add(new Portals(1500,128,128,128));
+        	for(i = 0; i<4;i++) {
+        		floorTilesList.add(new floorTiles(850+(32*i), 920, 32, 32));
+        	}
         	for(i=0;i<3;i++) {
+        		floorTilesList.add(new floorTiles(0+(i*32),1016,32,32));
+        		floorTilesList.add(new floorTiles(1100+(32*i), 850, 32, 32));
         		floorTilesList.add(new floorTiles(1515+(32*i), 256, 32, 32));
         	}
+        	portalList.add(new Portals(1500,128,128,128));
+
         }else if(level==2) {
             floorTilesList.add(new floorTiles(0, 1048, 32, 32));
             floorTilesList.add(new floorTiles(32, 1048, 32, 32));
@@ -99,6 +102,9 @@ public class Game implements Runnable{
 	}
 	public void update() {
         Player.update(floorTilesList,spikeList, portalList);
+	    for (Portals portal : portalList) {
+	    	portal.update();
+	    }
 	}
 	public void render(Graphics g) {
 		Backdrop.render(g);
