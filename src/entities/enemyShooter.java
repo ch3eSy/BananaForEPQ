cd3009530f6a09b734367a18e5841126d8d2a5b1
ch3eSy <maxcoutanche@gmail.com>
2024-01-30 09:36:53 +0000

    Added Classes for shooting enemy + bullet

--- src/entities/enemyShooter.java
+++ src/entities/enemyShooter.java
@@ -1,10 +1,73 @@
 package entities;
 
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
+
 public class enemyShooter extends entity{
-
-	public enemyShooter(int x, int y, int w, int h) {
-		super(x, y, w, h);
-		// TODO Auto-generated constructor stub
+	private BufferedImage[][] animations;
+    private int aniTick, aniIndex, aniSpd = 25;
+	private double OriginX, OriginY;
+    public enemyShooter(int x,int y,int w,int h) {
+		super(x,y,w,h);
+		OriginX = x;
+		OriginY=y;
+		portalsloadAnimations();
 	}
+	public void render(Graphics g) {
+		g.drawImage(animations[0][aniIndex],(int)posx,(int)posy,(int)width,(int)height,null);
+	}
+    private void portalsloadAnimations() {
+        InputStream is = getClass().getResourceAsStream("/snailsprites.png");
 
+        try {
+            BufferedImage img = ImageIO.read(is);
+            animations = new BufferedImage[3][6];
+
+            for (int j = 0; j < animations.length; j++)
+                for (int i = 0; i < animations[j].length; i++)
+                    animations[j][i] = img.getSubimage(i * 64, j * 64, 64, 64);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+	public void update() {
+		move();
+        updateAnimationTick();
+	}
+    private void move() {
+    	posx-=0.05;
+	}
+	private void updateAnimationTick() {
+        aniTick++;
+        if (aniTick >= aniSpd) {
+            aniTick = 0;
+            aniIndex++;
+            if (aniIndex >= 6) {
+                aniIndex = 0;
+            }
+        }
+    }
+	public Rectangle getHitbox() {
+		return new Rectangle((int)posx-5,(int)posy-5,96,128);
+	}
+	public void scroll(float speed,boolean scrollsnail) {
+		if(!scrollsnail) {
+			posx-=speed;
+		}
+	}
+	public void resetScroll() {
+		posx = OriginX;
+	}
+    public double getX() {
+        return posx;
+    }
+	public double getY() {
+		return posy;
+	}
 }
